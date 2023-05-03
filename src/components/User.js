5526df0a187c7f43ce0c9b5bc7065df84e20cb18
AntonLobanoff <anton28lobanov@gmail.com>
2023-05-03 13:15:07 +0300

    ok

---
+++ src/components/User.js
@@ -0,0 +1,39 @@
+import React from "react"
+import {IoCloseCircleSharp, IoHammerSharp} from 'react-icons/io5'
+import AddUser from "./AddUser"
+
+
+class User extends React.Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        editForm: false
+      }
+    }
+    user = this.props.user
+    render(){
+      return (
+        <div className="user" >
+            <IoCloseCircleSharp onClick={() => this.props.onDelete(this.user.id)} className="delete-icon"/>
+            <IoHammerSharp onClick={() => 
+              this.setState({
+                editForm: !this.state.editForm
+              })
+            } className="edit-icon"/>
+            <img src={this.user.avatar} alt=""/>
+              <h3>
+                {this.user.first_name} {this.user.last_name}
+              </h3>              
+              <p>{this.user.email}</p>              
+              <b>{this.user.isHappy ? "HAPPY" : "BAD"}</b>
+
+
+              {this.state.editForm && <AddUser onAdd={this.props.onEdit} user={this.user} />}
+            </div>
+      )
+  }
+    
+    
+  }
+
+  export default User
---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import React from "react"
+import * as ReactDOMClient from "react-dom/client"
+import App from './App'
+import './components/main.css'
+
+
+
+
+
+const app = ReactDOMClient.createRoot(document.getElementById("app"))
+app.render(<App />)
+
+
+
+
+
